f3dea5b9a700998b29e044bacf661542260e1d86
ginnsato <ginnsato@gmail.com>
2022-11-06 14:21:58 +0000

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jovyan/CSCI2270/project-moneymoves
+# Build directory: /home/jovyan/CSCI2270/project-moneymoves/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
